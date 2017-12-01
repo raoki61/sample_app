d63b312b8b6f33ee51a423cbaf35aa8ac3a04d11
Ray <r.aoki61@gmail.com>
2017-12-01 08:43:29 +0000

    Finish user registration

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,6 @@ SampleApp::Application.routes.draw do
   resources :users, only: [:show]
   root  'static_pages#home'
 
-  resources :users
-  devise_for :users
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
